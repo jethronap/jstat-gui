371933b60ef4915c50857eaadd9098b8deeed7ea
David Hilbert <a.giavaras@gmail.com>
2019-11-06 22:00:54 +0200

    #8 Add AnalysisFormWrapper class

---
+++ src/main/java/detail/wrappers/AnalysisFormWrapper.java
@@ -0,0 +1,15 @@
+package detail.wrappers;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class AnalysisFormWrapper {
+
+    @NotBlank(message="A dataSet is required")
+    public String dataSetName;
+
+    public String eda = null;
+    public String type;
+}
