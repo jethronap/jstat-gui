d44e8595edf3c2e5c7fa258c8d79162d9314ce87
Alex Giavaras <a.giavaras@gmail.com>
2019-12-21 16:16:38 +0200

    #8 Add compute package to wraps computing aspects

---
+++ src/main/java/detail/compute/DescriptiveStatistics.java
@@ -0,0 +1,15 @@
+package detail.compute;
+
+public class DescriptiveStatistics {
+
+    public double mean;
+    public double variance;
+    public double median;
+
+    public static <SampleTp> DescriptiveStatistics compute(SampleTp sample){
+
+        DescriptiveStatistics statistics = new DescriptiveStatistics();
+
+        return statistics;
+    }
+}
