71c79783110a0f89d19333ce6b56cd928ef1e939
jethronap <efthioa@gmail.com>
2020-02-02 14:43:06 +0200

    #24 added numeric for sample type & compute method

--- src/main/java/detail/compute/DescriptiveStats.java
+++ src/main/java/detail/compute/DescriptiveStats.java
@@ -1,23 +1,31 @@
 package detail.compute;
 
-
+import detail.compute.NumericSample;
+import detail.datasets.IDataSet;
+import detail.datasets.TableDataSet;
 import detail.wrappers.StatisticsWrapper;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.apache.commons.math3.stat.descriptive.rank.Median;
+import tech.tablesaw.columns.Column;
 
 public class DescriptiveStats {
 
     public String name;
     public StatisticsWrapper statisticsWrapper;
+    //public IDataSet dataSet;
+    //public Column sample;
 
-    public <SampleTp> void compute(SampleTp sample) {
+    public void compute(NumericSample sample) {
 
-        DescriptiveStatistics statistics = new DescriptiveStatistics();
+        sample.getStatistics();
+        //this.sample = sample;
+        //sample = new NumericSample(dataSet.getName(), 0);
+//        DescriptiveStatistics statistics = new DescriptiveStatistics();
+//        this.statisticsWrapper.mean = statistics.getMean();
+//        this.statisticsWrapper.variance = statistics.getVariance();
+//        this.statisticsWrapper.median = new Median().evaluate();
 
-        this.statisticsWrapper.mean = statistics.getMean();
-        this.statisticsWrapper.variance = statistics.getVariance();
-        //  this.median = new Median().evaluate();
 
-
+  //      sample.getStatistics();
     }
 }
