db28beb30980144a74bfe0620889ce1f8a560517
Alex Giavaras <a.giavaras@gmail.com>
2020-02-12 13:25:04 +0200

    #29 Add EDAResultModel and ComputationModelType enumeration

---
+++ src/main/java/detail/compute/ComputationModelType.java
@@ -0,0 +1,14 @@
+package detail.compute;
+
+/**
+ * Enumeration for the computation models that
+ * JStat supports
+ */
+public enum ComputationModelType {
+
+    EDA,
+    LINEAR_REGRESSION,
+    LOGISTIC_REGRESSION,
+    CLUSTERING,
+    INVALID_MODEL // keep this always last
+}
