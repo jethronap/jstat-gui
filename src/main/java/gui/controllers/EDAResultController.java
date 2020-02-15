9d4a3611d82a960b486871ef4628a87d07b2d68b
jethronap <efthioa@gmail.com>
2020-02-15 16:10:16 +0200

    #24 upadated get mapping & added model

--- src/main/java/gui/controllers/EDAResultController.java
+++ src/main/java/gui/controllers/EDAResultController.java
@@ -1,135 +1,76 @@
 package gui.controllers;
 
 
-import detail.compute.DescriptiveStats;
-import detail.config.JStatGuiGlobalData;
-import detail.datasets.DataSetViewInfoHolder;
-import detail.datasets.IDataSet;
-import detail.tasks.ComputeDescriptiveStatisticsTask;
-import detail.tasks.TaskBase;
 import detail.wrappers.AnalysisFormWrapper;
+import mongodb.ComputeTasksControllerDoc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
-@RequestMapping("/analysis-result")
-public class AnalysisResult {
+@RequestMapping("eda_view_results")
+public class EDAResultController {
 
-    TaskBase task = null;
-    List<DescriptiveStats> result;
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Value("${collections.control.tasks}")
+    String controlCollection;
+
     @GetMapping
-    public String analysisView(@RequestParam("taskName") String taskName, Model model) {
+    public String analysisView(@RequestParam("controlTaskId") String controlTaskId, Model model) {
 
-        //String taskIdentifier =
+        // find the document that corresponds
+        // to this control Task
+        ComputeTasksControllerDoc doc = mongoTemplate.findById(controlTaskId, ComputeTasksControllerDoc.class, controlCollection);
 
-        task = JStatGuiGlobalData.getTask(taskName);
-
-        if (task != null) {
-            System.out.println("Task with name: " + taskName + " exists");
-        } else {
-            System.out.println("Task with name: " + taskName + " does not exist");
+        if(doc == null){
+            throw new IllegalArgumentException("No control task for id: "+controlTaskId);
         }
 
-        if (result == null) {
-
-            model.addAttribute("taskName", taskName);
-
+        // if we haven't finished return
+        if(!doc.isFinished()){
+            model.addAttribute("controlTaskId", controlTaskId);
+            // msg to hit f5 prompt user to refresh somehow.
+            return "eda_view_results";
         }
-        else {
-            for (int i = 0; i < result.size(); i++) {
-                model.addAttribute("mean", result.get(i).mean);
-            }
-        }
+        /*else{
+            model.addAttribute("controlTaskId", controlTaskId );
+            return "analysis_result_descriptive_stats";
+        }*/
 
-        return "eda_results";
+
+        model.addAttribute("controlTaskId", controlTaskId );
+
+        return "eda_view_results";
     }
 
+    // TODO: probably mundane & should be removed
     @PostMapping
-    public String handleAnalysisResultForm(@RequestParam("taskName") String taskName, AnalysisFormWrapper formWrapper /*Errors errors*/) throws Exception {
+    public String handleAnalysisResultForm(@RequestParam("controlTaskId") String controlTaskId, AnalysisFormWrapper formWrapper /*Errors errors*/) throws Exception {
 
 
-        task = JStatGuiGlobalData.getTask(taskName);
+        ComputeTasksControllerDoc doc = mongoTemplate.findById(controlTaskId, ComputeTasksControllerDoc.class, controlCollection);
 
-        System.out.println(formWrapper.colName);
-        System.out.println(taskName);
-
-        if (taskName != null) {
-
-            System.out.println("we got here.");
-            //System.out.println(task.call());
-
-            if (task == null) {
-                return "analysis_result_descriptive_stats";
-            }
-
-            result =((ComputeDescriptiveStatisticsTask) task).getResult();
-            System.out.println("result size: "+ result.size());
-            return "redirect:/analysis-result?taskName=" + taskName;
-           // model.addAttribute("mean");
-//          model.addAttribute("median");
-//            model.addAttribute("variance");
-        } else if (formWrapper.linear_regression != null) {
-
-        } else if (formWrapper.non_linear_regression != null) {
-
-        } else if (formWrapper.logistic_regression != null) {
-
-        } else if (formWrapper.kmeans_clustering != null) {
-
-        } else {
-
-            // return an error message that no valid action
-            // was selected
+        if(doc == null){
+            throw new IllegalArgumentException("No control task for id: "+controlTaskId);
         }
-
-        // redirect to the analysis page again
-        return "index";
+        return "redirect:/eda_view_results?controlTaskId="+controlTaskId;
     }
-
-    protected void computeDataSetStatistics(AnalysisFormWrapper formWrapper) {
-
-
-        System.out.println("================");
-        System.out.println("computeDataSetStatistics...");
-        System.out.println("================");
-        System.out.println("Filename exploreDataSet: " + formWrapper.eda);
-        System.out.println("Filename exploreDataSet: " + formWrapper.dataSetName);
-        System.out.println("Column Name: " + formWrapper.colName);
-
-        // get the name of the dataset
-        String dataSetName = formWrapper.dataSetName;
-        IDataSet dataSet = JStatGuiGlobalData.dataSetContainer.getDataSet(dataSetName);
-
-        if (formWrapper.colName == "All") {
-
-            List<String> dataSetCols = dataSet.getColumnNames();
-            String[] names = new String[dataSetCols.size()];
-
-            for (int i = 0; i < names.length; ++i) {
-                names[i] = dataSetCols.get(i);
-            }
-
-            // submit it to the pool
-            TaskBase task = new ComputeDescriptiveStatisticsTask("EDA", dataSet, names);
-            JStatGuiGlobalData.workersPool.submit(task);
-            JStatGuiGlobalData.tasks.add(task);
-
-
-        } else {
-
-            // submit it to the pool
-            TaskBase task = new ComputeDescriptiveStatisticsTask("EDA", dataSet, formWrapper.colName);
-            JStatGuiGlobalData.workersPool.submit(task);
-            JStatGuiGlobalData.tasks.add(task);
-        }
-    }
+//    private EDAResultModel getResults(TaskBase task){
+//
+//        /**
+//         * i want to retrieve ids which are controlled by ComputeTasksControllerDoc
+//         * for every such id i want the resultDoc from the db
+//         * the put the model to serve them to the frontend
+//         */
+//
+//        return EDAResultModel ;
+//    }
 }
