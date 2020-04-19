0caeb170ca6a7ecc3ce0f4d4ea2ac646631201a2
jethronap <efthioa@gmail.com>
2020-04-19 20:12:16 +0300

    #24 added result model list & iterated through results Map

--- src/main/java/gui/controllers/EDAResultController.java
+++ src/main/java/gui/controllers/EDAResultController.java
@@ -17,6 +17,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("eda_view_results")
 public class EDAResultController {
@@ -53,9 +58,31 @@ public class EDAResultController {
 
 
         model.addAttribute("controlTaskId", controlTaskId );
-        model.addAttribute("mean", getResults(controlTaskId).mean);
-        model.addAttribute("median", getResults(controlTaskId).median);
-        model.addAttribute("variance", getResults(controlTaskId).variance);
+        //EDAResultModel resultModel = getResults(controlTaskId);
+        List<EDAResultModel> resultModelList = getResults(controlTaskId);
+        Map<String, Map<String, Double>> columns = new HashMap<>();
+
+        for (int i = 0; i < resultModelList.size(); i++) {
+            EDAResultModel edaResultModel = resultModelList.get(i);
+
+            Map<String, Double> values = new HashMap<>();
+
+            values.put("mean", edaResultModel.mean);
+            values.put("variance", edaResultModel.variance);
+            values.put("median", edaResultModel.median);
+
+            columns.put(edaResultModel.name, values);
+            System.out.println(edaResultModel.name);
+//            columns.put("weight", values);
+//            model.addAttribute("mean", resultModel.mean);
+//            model.addAttribute("median", resultModel.median);
+//            model.addAttribute("variance", resultModel.variance);
+//
+        }
+
+        model.addAttribute("controlTaskId", controlTaskId );
+        model.addAttribute("columns", columns);
+
 
         return "eda_view_results";
     }
@@ -74,18 +101,32 @@ public class EDAResultController {
     }
 
 
-    private EDAResultModel getResults(String controlTaskId){
+    private List<EDAResultModel> getResults(String controlTaskId){
         
         /**
          * i want to retrieve ids which are controlled by ComputeTasksControllerDoc
          * for every such id i want the resultDoc from the db
          * the put the model to serve them to the frontend
          */
-        DescriptiveStatisticsResultDoc doc = mongoTemplate.findById(controlTaskId, DescriptiveStatisticsResultDoc.class, controlCollection);
-        edaResultModel.mean = doc.getMean();
-        edaResultModel.median = doc.getMedian();
-        edaResultModel.variance = doc.getVariance();
+        ComputeTasksControllerDoc doc = mongoTemplate.findById(controlTaskId, ComputeTasksControllerDoc.class, controlCollection);
+        System.out.println(doc);
+        List<String> taskIds = doc.getTaskIds();
+        List<EDAResultModel> resultModelList = new ArrayList<EDAResultModel>();
 
-        return edaResultModel ;
+        for (int i = 0; i < taskIds.size(); i++) {
+            String id = taskIds.get(i);
+            DescriptiveStatisticsResultDoc descriptiveStatisticsResultDoc = mongoTemplate.findById(id,
+                    DescriptiveStatisticsResultDoc.class, "compute_results_coll");
+            EDAResultModel resultModel = descriptiveStatisticsResultDoc.getResultModel();
+
+//            resultModel.name = descriptiveStatisticsResultDoc.getName();
+//            resultModel.mean = descriptiveStatisticsResultDoc.getMean();
+//            resultModel.median = descriptiveStatisticsResultDoc.getMedian();
+//            resultModel.variance = descriptiveStatisticsResultDoc.getVariance();
+            resultModelList.add(resultModel);
+        }
+
+
+        return resultModelList ;
     }
 }
